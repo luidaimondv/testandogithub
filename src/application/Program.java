6d048b94ec37efdf8e8bb4ea85bcf1e46d142100
Lui Daimon <luidaimondv@gmail.com>
2024-06-28 10:38:46 -0300

    Adicionando um exemplo soma test

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,10 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello guys!!!!!");
+		
+		int a = 0;
+		int b = a;
+		System.out.println(a + b);
 	}
 
 }
